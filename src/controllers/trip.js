8eabc7630cb79d11b4864698df3cf6aa473b1450
MamaedovArif <arifmho@yandex.ru>
2020-04-20 13:42:25 +0300

    наладили возврат everything

--- src/controllers/trip.js
+++ src/controllers/trip.js
@@ -7,7 +7,7 @@ import ListOfDaysComponent, {generateDays} from '../components/list-trips.js';
 // import {defaultData} from '../mock/route-point.js';
 import PointOfRouteComponent from '../components/route-point.js';
 import {render, RenderPosition, replace} from '../utils/render.js'; // , remove
-import {filterComponent} from '../main.js';
+import {filterComponent} from '../main.js'; // кустарно
 import {FilterType} from '../components/filters.js';
 /*
 const createDefaultForm = (button, container) => {
@@ -122,15 +122,26 @@ export default class TripController {
       const filteredPoints = getFilteredPoints(allPoints, filterType);
       const parentList = this._container.querySelector(`.trip-days`);
       parentList.innerHTML = ``;
-      //  выровниваем
-      parentList.insertAdjacentHTML(`beforeend`, generateDays({date: new Date()}, 0)); // *
-      parentList.querySelector(`.day__counter`).textContent = ``; // *
-      parentList.querySelector(`.day__date`).textContent = ``; // *
+      //  выравниваем
+      if (filterType !== FilterType.EVERYTHING) {
+        parentList.insertAdjacentHTML(`beforeend`, generateDays({date: new Date()}, 0)); // *
+        parentList.querySelector(`.day__counter`).textContent = ``; // *
+        parentList.querySelector(`.day__date`).textContent = ``; // *
 
-      for (let x = 0; x < filteredPoints.length; x++) {
-        let point = filteredPoints[x];
-        // renderPoint(this._container.querySelector(`.trip-days`), point); предыдущий вариант
-        renderPoint(this._container.querySelector(`.trip-events__list`), point); // выровниваем
+        for (let x = 0; x < filteredPoints.length; x++) {
+          let point = filteredPoints[x];
+          // renderPoint(this._container.querySelector(`.trip-days`), point); предыдущий вариант
+          renderPoint(this._container.querySelector(`.trip-events__list`), point); // выравниваем
+        }
+      } else {
+        render(this._container, new ListOfDaysComponent(days), RenderPosition.BEFOREEND);
+        let listOfDays = this._container.querySelectorAll(`.trip-events__list`); // *
+        for (let x = 0; x < allPoints.length; x++) {
+          let points = allPoints[x];
+          for (let y = 0; y < points.length; y++) {
+            renderPoint(listOfDays[x], points[y]);
+          }
+        }
       }
     });
   }
