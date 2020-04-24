884b00ed52e34e9d0c327683d5be054f1ad4f0d9
MamaedovArif <arifmho@yandex.ru>
2020-04-24 18:47:11 +0300

    доделываем автоматическое закрытие

--- src/controllers/point.js
+++ src/controllers/point.js
@@ -2,17 +2,27 @@ import PointOfRouteComponent from '../components/route-point.js';
 import FormForEditComponent from '../components/editing-form.js';
 import {render, RenderPosition, replace} from '../utils/render.js';
 
+const Mode = {
+  DEFAULT: `default`,
+  EDIT: `edit`,
+};
 
 export default class PointController {
-  constructor(container, onDataChange) {
+  constructor(container, onDataChange, onViewChange) {
     this._container = container;
     this._pointOfRouteComponent = null;
     this._formForEditComponent = null;
     this._onDataChange = onDataChange;
 
+    this._onViewChange = onViewChange;
+    this._mode = Mode.DEFAULT;
+
     this._onEscKeyDown = this._onEscKeyDown.bind(this);
   }
   render(dataOfRoute) {
+    const oldPointComponent = this._pointOfRouteComponent;
+    const oldPointEditComponent = this._formForEditComponent; /// !!!!
+
     this._pointOfRouteComponent = new PointOfRouteComponent(dataOfRoute);
     this._formForEditComponent = new FormForEditComponent(dataOfRoute);
 
@@ -27,17 +37,29 @@ export default class PointController {
       document.removeEventListener(`keydown`, this._onEscKeyDown);
     });
 
-    this._formForEditComponent.setFavoriteChangeHandler(() => { //onDataChange
+    this._formForEditComponent.setFavoriteChangeHandler(() => {
       //this.setAttribute(`checked`, true);
       this._onDataChange(this, dataOfRoute, Object.assign({}, dataOfRoute, {
         isFavorite: !dataOfRoute.isFavorite,
       }))
-    }); // !!!
+    });
 
-    render(this._container, this._pointOfRouteComponent, RenderPosition.BEFOREEND);
+    if (oldTaskEditComponent && oldTaskComponent) { /// !!!
+      replace(this._taskComponent, oldTaskComponent);
+      replace(this._taskEditComponent, oldTaskEditComponent);
+    } else {
+      render(this._container, this._pointOfRouteComponent, RenderPosition.BEFOREEND);
+    }
+  }
+
+  setDefaultView() {
+    if (this._mode !== MODE.DEFAULT) {
+      this._replaceFormToPoint();
+    }
   }
 
   _replacePointToForm() {
+    this._onViewChange();
     replace(this._formForEditComponent, this._pointOfRouteComponent);
   };
 
